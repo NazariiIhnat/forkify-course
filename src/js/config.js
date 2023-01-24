81a56213393ecebe7f2787aa9560f1479d4f7301
NazariiIhnat <ignatnazar@gmail.com>
2023-01-24 18:17:25 +0100

    Forkify app

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'fa550ba5-d6f9-44ea-9a68-154fd7ddfc8b';
+export const MODAL_CLOSE_SEC = 2.5;
